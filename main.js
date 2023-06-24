5bda317a3afbd58522543b85b80479bcaca583b2
varshagm119 <varshagm96@gmail.com>
2023-06-25 00:17:37 +0530

    filter with new description box

--- main.js
+++ main.js
@@ -11,30 +11,44 @@ function addIem(e){
     e.preventDefault();
 
     var newItem = document.getElementById('item').value;
+    var newDescription = document.getElementById('item2').value;
+
+    var newString = newItem.concat(" ",newDescription);
+
    // console.log(newItem);
    var li = document.createElement('li');
    li.className = 'list-group-item';
-   li.appendChild(document.createTextNode(newItem));
-   //console.log(li)
+   li.appendChild(document.createTextNode(newString));
+   
 
     //adding the delete button
     var deleteBtn = document.createElement('button');
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
     deleteBtn.appendChild(document.createTextNode('X'));
-   // console.log(deleteBtn)
+   
 
     //adding an edit button
     var editBtn = document.createElement('button');
     editBtn.className = 'btn btn-sm float-right';
     editBtn.appendChild(document.createTextNode('Edit'));
     editBtn.style.borderBottom = 'solid 2px';
+    
+    var eachList = document.getElementsByTagName('li');
+   // console.log(eachList);
+   
+   
+
+
+   // console.log(li)
+    li.appendChild(deleteBtn);
     li.appendChild(editBtn);
-
-
-   li.appendChild(deleteBtn);
-
     itemList.appendChild(li);
     document.getElementById('item').value = '';
+    document.getElementById('item2').value = '';
+
+
+
+
 
 }
 
@@ -51,5 +65,35 @@ function deleteItem(e){
             itemList.removeChild(li);
         }
     }
+
+}
+
+
+//for filtering
+var filter = document.getElementById('filter');
+
+filter.addEventListener('keyup',filterItem);
+
+function filterItem(e){
+
+    var text = e.target.value.toLowerCase();
+
+    //get the list
+    var ite = itemList.getElementsByTagName('li');
+
+    //convert the htm collectables to an array
+    Array.from(ite).forEach(function(itemText){
+        console.log(itemText)
+        var text1 = itemText.firstChild.textContent;
+        
+        var itemName = text1
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            itemText.style.display = 'block';
+        }
+        else{
+            itemText.style.display = 'none';
+        }
+
+    })
 
 }
